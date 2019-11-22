9592d20e8ccd3fbcef7b27f791030b4012c3aec5
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-11-22 08:13:28 -0300

    .env.example

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '.ljlhLl dfG_=F#h $$ a@df ##$@@@ () h1hlk0 303 3=+=457340952-94',
-  expireIn: '7d'
+  secret: process.env.TOKEN_SECRET,
+  expireIn: process.env.TOKEN_EXPIRATION
 };
--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,17 @@
 module.exports = {
-  dialect: 'mariadb',
-  host: 'localhost',
-  port: '3306',
-  username: 'root',
-  password: 'senha',
-  database: 'gympoint',
+  dialect: process.env.DATABASE_DIALECT,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true
   },
   dialectOptions: {
-    timezone: 'America/Sao_Paulo'
+    timezone: process.env.TZ
   },
-  timezone: 'America/Sao_Paulo'
+  timezone: process.env.TZ
 };
